f4b18dafa7aa45cdfc21f8452ea9b02c07d5930e
faithakinbobola <faithakinbobola2@gmail.com>
2025-07-13 19:33:14 +0100

    Advanced types Part 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,9 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience: number,
-    location: string,
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience: number;
+    location: string;
     [propName: string]: any;
 }
 
@@ -15,6 +15,33 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
+interface StudentConstructor {
+    new (firstName: string, lastName:string): StudentInterface;
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
 }
