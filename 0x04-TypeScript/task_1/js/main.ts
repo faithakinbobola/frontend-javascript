8d5081e592271c87c82b26df3c84b178b5be6d8d
faithakinbobola <faithakinbobola2@gmail.com>
2025-07-13 21:14:23 +0100

    updates

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,34 +1,34 @@
-interface Teacher {
+export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience: number;
+    yearsOfExperience?: number;
     location: string;
     [propName: string]: any;
 }
 
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-interface printTeacherFunction {
+export interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-interface StudentConstructor {
+export interface StudentConstructor {
     new (firstName: string, lastName:string): StudentInterface;
 }
 
-interface StudentInterface {
+export interface StudentInterface {
     workOnHomework(): string;
     displayName(): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-class StudentClass implements StudentInterface {
+export  class StudentClass implements StudentInterface {
     firstName: string;
     lastName: string;
 
